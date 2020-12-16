04a9391889c4d4c3a6ff6ca68d9ee0d355593976
lilsherlock <potatogames15@gmail.com>
2020-12-16 20:03:33 +0000

    update

--- console.py
+++ console.py
@@ -4,8 +4,10 @@ import cmd
 import sys
 from models.base_model import BaseModel
 from models.__init__ import storage
+
 from models.user import User
 from models.place import Place
+from models.engine.file_storage import FileStorage
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
@@ -21,81 +23,13 @@ class HBNBCommand(cmd.Cmd):
     """ Contains the functionality for the HBNB console"""
 
     # determines prompt for interactive/non-interactive modes
-    prompt = '(hbnb) ' if sys.__stdin__.isatty() else ''
+    prompt = '(hbnb) '
 
     classes = {
                'BaseModel': BaseModel, 'User': User, 'Place': Place,
                'State': State, 'City': City, 'Amenity': Amenity,
                'Review': Review
               }
-    dot_cmds = ['all', 'count', 'show', 'destroy', 'update']
-    types = {
-             'number_rooms': int, 'number_bathrooms': int,
-             'max_guest': int, 'price_by_night': int,
-             'latitude': float, 'longitude': float
-            }
-
-    def preloop(self):
-        """Prints if isatty is false"""
-        if not sys.__stdin__.isatty():
-            print('(hbnb)')
-
-    def precmd(self, line):
-        """Reformat command line for advanced command syntax.
-
-        Usage: <class name>.<command>([<id> [<*args> or <**kwargs>]])
-        (Brackets denote optional fields in usage example.)
-        """
-        _cmd = _cls = _id = _args = ''  # initialize line elements
-
-        # scan for general formating - i.e '.', '(', ')'
-        if not ('.' in line and '(' in line and ')' in line):
-            return line
-
-        try:  # parse line left to right
-            pline = line[:]  # parsed line
-
-            # isolate <class name>
-            _cls = pline[:pline.find('.')]
-
-            # isolate and validate <command>
-            _cmd = pline[pline.find('.') + 1:pline.find('(')]
-            if _cmd not in HBNBCommand.dot_cmds:
-                raise Exception
-
-            # if parantheses contain arguments, parse them
-            pline = pline[pline.find('(') + 1:pline.find(')')]
-            if pline:
-                # partition args: (<id>, [<delim>], [<*args>])
-                pline = pline.partition(', ')  # pline convert to tuple
-
-                # isolate _id, stripping quotes
-                _id = pline[0].replace('\"', '')
-                # possible bug here:
-                # empty quotes register as empty _id when replaced
-
-                # if arguments exist beyond _id
-                pline = pline[2].strip()  # pline is now str
-                if pline:
-                    # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
-                            and type(eval(pline)) is dict:
-                        _args = pline
-                    else:
-                        _args = pline.replace(',', '')
-                        # _args = _args.replace('\"', '')
-            line = ' '.join([_cmd, _cls, _id, _args])
-
-        except Exception as mess:
-            pass
-        finally:
-            return line
-
-    def postcmd(self, stop, line):
-        """Prints if isatty is false"""
-        if not sys.__stdin__.isatty():
-            print('(hbnb) ', end='')
-        return stop
 
     def do_quit(self, command):
         """ Method to exit the HBNB console"""
@@ -118,6 +52,11 @@ class HBNBCommand(cmd.Cmd):
         """ Overrides the emptyline method of CMD """
         pass
 
+    def do_delete(self, args):
+        """ delete object """
+        if not args:
+            print("add something")
+        FileStorage.delete(args)
     def do_create(self, args):
         """ Create an object of any class"""
         arguments = []
@@ -127,7 +66,6 @@ class HBNBCommand(cmd.Cmd):
             return
         elif arguments[0] not in HBNBCommand.classes:
             print("** class doesn't exist **")
-            print(len(arguments))
             return
         new_instance = HBNBCommand.classes[arguments[0]]()
         length = len(arguments)
@@ -147,7 +85,7 @@ class HBNBCommand(cmd.Cmd):
                 setattr(new_instance, sub_argument[0], sub_argument[1])
                 x += 1
         new_instance.save()
-        print(new_instance)
+        print(new_instance.id)
 
     def help_create(self):
         """ Help information for the create method """
