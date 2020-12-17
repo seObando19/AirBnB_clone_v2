5ed0faceb421a5e863ca31d29243b2eb12d0028c
seObando19 <sebastiano-10@hotmail.com>
2020-12-17 18:46:11 -0500

    Completed task 7 and 8

--- models/city.py
+++ models/city.py
@@ -1,18 +1,14 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String, ForeignKey
-from models.place import Place
-from os import environ
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
 
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    if  environ.get('HBNB_TYPE_STORAGE') == "db":
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship("Place", cascade="all, delete",  backref="cities")
-    else:
-        name = ""
-        state_id = ""
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", backref="cities")
--- models/user.py
+++ models/user.py
@@ -1,24 +1,18 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
-from models.place import Place
-from os import environ
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
 
     __tablename__ = "users"
-    if environ.get('HBNB_TYPE_STORAGE') == "db":
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=False)
-        last_name = Column(String(128), nullable=False)
-        places = relationship("Place", cascade="all, delete", backref="user")
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=False)
+    last_name = Column(String(128), nullable=False)
+    places = relationship("Place", backref="user")
