32f0e5bf9720872eafb028066e62b28223fe6da5
seObando19 <sebastiano-10@hotmail.com>
2021-01-19 23:13:41 +0000

    Updated task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -55,9 +55,10 @@ def printTextPython(text="is cool"):
 def isNumber(n):
     return "{} is a number".format(n)
 
+
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def showTemplate(n):
-   return render_template('5-number.html', n=n)
+    return render_template('5-number.html', n=n)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
